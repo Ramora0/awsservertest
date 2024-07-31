f60ec17cb1232e94bd9e6515498a6fcc73c3f6f3
Ramora0 <99ilovecats99@gmail.com>
2024-07-31 09:27:57 -0400

    Test

--- src/main/java/com/leedavis/Constants.java
+++ src/main/java/com/leedavis/Constants.java
@@ -2,7 +2,7 @@ package com.leedavis;
 
 public class Constants {
   public static final String REGISTER_EVENT = "event.tag.report";
-  public static final long SLEEP_TIME = 1000 * 60;
+  public static final long SLEEP_TIME = 1000 * 60 + 1;
   public static final String READER_IP = "72.221.12.244";// "192.168.250.110";//
   public static final String TABLE_NAME = "wasabi-rfid";
   public static final String HISTORY_TABLE_NAME = "wasabi-rfid-history";
